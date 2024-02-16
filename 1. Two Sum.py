916bd9a5ac62756d5b2da98bd21f2ecd0851c025
Nirakshi Kundu <96015607+nirakshi2003@users.noreply.github.com>
2024-02-16 20:30:45 +0530

    Add files via commit

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -5,10 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        seen={}
-        for i, num in enumerate(nums):
-            complement=target-num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num]=i
-        return []
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i]+nums[j]==target:
+                    return i, j
+        return None
