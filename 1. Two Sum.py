d1945e7d6a1d984451f3c76b30323b8012c40cac
Nirakshi Kundu <96015607+nirakshi2003@users.noreply.github.com>
2024-02-16 20:31:14 +0530

    Add files via upload

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -10,4 +10,4 @@ class Solution(object):
             for j in range(i+1, n):
                 if nums[i]+nums[j]==target:
                     return i, j
-        return None
+        return None 
